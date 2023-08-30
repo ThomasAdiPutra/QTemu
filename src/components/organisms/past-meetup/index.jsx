0b251172b2221f4c2caaf16abd88382e7342bd77
Thomas <thomasadiputra@gmail.com>
2023-08-30 20:58:14 +0700

    Fix responsive

--- src/components/organisms/past-meetup/index.jsx
+++ src/components/organisms/past-meetup/index.jsx
@@ -7,9 +7,9 @@ export default function PastMeetup({ data }) {
   return (
     <div id='past-meetup'>
       <ContainerHeader title={'Past Meetup'} seeAll={true}/>
-      <div className="flex justify-between ps-5">
+      <div className="flex justify-between ps-5 lg:space-x-6 flex-wrap ">
       {data?.map((item, index) => (
-        <Card key={index} title={ item.title } className={`flex-1 ${ index === 1 ? 'mx-20': ''}`}>
+        <Card key={index} title={ item.title } className='flex-1 md:me-2 sm:my-2 me-1'>
           <p className='font-bold'>{ item.body }</p>
           <div className='my-5'>
             <span>{item.count}</span> &nbsp;
