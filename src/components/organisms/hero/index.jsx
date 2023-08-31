6a10eaa4e15e8c5ffd0b9acb951801c85d366c2a
Thomas <thomasadiputra@gmail.com>
2023-08-31 08:53:04 +0700

    Update data

--- src/components/organisms/hero/index.jsx
+++ src/components/organisms/hero/index.jsx
@@ -13,19 +13,19 @@ export default function Hero({ title, location, members, organizer }) {
         />
         <div className='flex flex-col flex-1 ms-5 justify-between'>
           <p className='text-lg font-bold'>{title}</p>
-          <div className="flex flex-col md:flex-row">
-            <div className="md:w-2/12 font-bold">Location</div>
-            <div className="w-full md:w-10/12">{location}</div>
+          <div className="flex flex-row">
+            <div className="w-4/12 md:w-3/12 lg:w-1/12 font-bold">Locations</div>
+            <div className="sm:w-10/12">{location}</div>
           </div>
 
-          <div className="flex flex-col md:flex-row">
-            <div className="md:w-2/12 font-bold">Members</div>
-            <div className="w-full md:w-10/12">{members}</div>
+          <div className="flex flex-row">
+            <div className="w-4/12 md:w-3/12 lg:w-1/12 font-bold">Members</div>
+            <div className="sm:w-10/12">{members}</div>
           </div>
 
-          <div className="flex flex-col md:flex-row">
-            <div className="md:w-2/12 font-bold">Organizer</div>
-            <div className="w-full md:w-10/12">{organizer}</div>
+          <div className="flex flex-row">
+            <div className="w-4/12 md:w-3/12 lg:w-1/12 font-bold">Organizer</div>
+            <div className="sm:w-10/12">{organizer}</div>
           </div>
 
           <div>
